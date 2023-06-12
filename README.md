adec6a13ae55b619257379c8f2e84b47f1bd2625
郭 軍 <976849990@qq.com>
2023-06-12 10:20:01 +0900

    refs gj0235

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Github ID list 
 
 1> liuyang: lyang777
+2> guojun: guojun200
--- main.c
+++ main.c
@@ -2,6 +2,8 @@ main()
 {
     //liuyang
     int ly0054 = function_ly0054();
+    //guojun
+    int gj0054 = function_gj0235();
 
 
     //TBD
