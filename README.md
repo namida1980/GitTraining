68456f11d056a62eb2cd27a2962b06473ad73356
acni 李俊儀 <lijunyiyoooo@126.com>
2023-06-13 12:26:59 +0900

    commit by ljy

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1> liuyang: lyang777
 2> guojun: guojun200
+3> ljy:lijunyi0244
--- main.c
+++ main.c
@@ -4,7 +4,8 @@ main()
     int ly0054 = function_ly0054();
     //guojun
     int gj0054 = function_gj0235();
-
+    //lijunyi
+    int ljy0244 = function_ljy0244();
 
     //TBD
 }
